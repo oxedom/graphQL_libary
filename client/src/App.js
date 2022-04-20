ac1e21d42583d025d5d600796e62f062f1825cf5
Sam <36157647+oxedom@users.noreply.github.com>
2022-04-20 11:46:36 +0300

    uninstalled conflicts apollo packages, created set up for ApolloClient on App.JS and book comp

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
-
+import { ApolloProvider } from 'react-apollo'
+import ApolloClient from 'apollo-boost';
 //Comps
 import BookList from "./comps/BookList";
 
+const client = new ApolloClient({ uri: 'http://localhost:4000/graphql'})
+
 function App() {
   return (
-    <div id="main">
-      <BookList></BookList>
-      <h1> kek </h1>
-    </div>
+      <ApolloProvider client={client}>
+      <div id="main">
+        <BookList></BookList>
+      </div>
+      // </ApolloProvider>
   );
 }
 
--- client/src/comps/BookList.js
+++ client/src/comps/BookList.js
@@ -1,9 +1,21 @@
-const BookList = () => {
+import { gql } from 'apollo-boost';
+import { graphql } from 'react-apollo';
+const getBooksQuery = gql`
+{
+    books{
+        name
+        id
+    }
+}
+`
+const BookList = (props) => {
+
+    console.log(props)
     return ( <div>
      <ul id="book-list">
          <li> book name </li>
      </ul>
-    </div>  );
+    </div> );
 }
  
-export default BookList;
+export default graphql(getBooksQuery)(BookList);

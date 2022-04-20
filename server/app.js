4d1daaa3f7ef725403ad69131017be24a6053531
Sam <36157647+oxedom@users.noreply.github.com>
2022-04-20 11:55:59 +0300

    changes to package json and app.js

--- server/app.js
+++ server/app.js
@@ -4,14 +4,19 @@ const { graphqlHTTP } = require('express-graphql');
 const { graphql } = require('graphql')
 const schema = require('./schema/schema')
 const app = express()
-const port = 7999
+const port = 4000
 const config = require('./config/config')
 
+//alow cross-origin requests
+const cors = require('cors')
+app.use(cors)
+
+//connect to db 
 mongoose.connect(config)
 mongoose.connection.once('open', () => { console.log('Connected to DB')})
 app.use('/graphql', graphqlHTTP({
     schema,
-    graphiql: true
+    graphiql: false
 }));
 
 
